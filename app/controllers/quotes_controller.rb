52537211ecd5e3b37f56f89ee369f2ee977d5e38
Avneesh Sawhney <avneesh.sawhney@pepperdine.edu>
2019-02-24 08:27:34 +0000

    Completed about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -10,9 +10,18 @@ end
 
 
 def create
-Quote.create(quote_params)
+@quote = Quote.create(quote_params)
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
+
 redirect_to root_path
 end
+ 
+ def about
+  end
+
+
  private
 
   def quote_params
